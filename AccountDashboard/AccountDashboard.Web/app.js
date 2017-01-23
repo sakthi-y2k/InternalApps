ddcbf7a5f9fae133fb158dc27411111c45a009f8
sakthi-y2k <srisakthi2605@gmail.com>
2017-01-23 18:34:14 +0530

    Adding routing

--- AccountDashboard/AccountDashboard.Web/app.js
+++ AccountDashboard/AccountDashboard.Web/app.js
@@ -1,3 +1,16 @@
 ﻿(function () {
-    var app = angular.module("AccountDashboard", []);
+    var app = angular.module("AccountDashboard", ["ngRoute"]);
+
+    app.config(function ($routeProvider) {
+        $routeProvider
+            .when("/", {
+                templateUrl: "views/dashboard.html",
+                controller: "MainController"
+            })
+             //.when("/", {
+             //    templateUrl: "views/upload.html",
+             //    controller: ""
+             //})
+           .otherwise({ redirectTo: "/" });
+    });
 }());
